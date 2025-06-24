2b95cbe7ddd92d62e5d65604f301ba4c53e260e0
kostabkpark <kostabkpark@gmail.com>
2025-06-24 16:44:25 +0900

    workflow update

--- src/main/java/org/example/cicd_test3/controller/MainController.java
+++ src/main/java/org/example/cicd_test3/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
   @GetMapping
   public String hello() {
-    return "This is auto deployment with container";
+    return "This is auto deployment with container - 2";
   }
 }
